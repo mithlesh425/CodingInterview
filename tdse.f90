6cb5f8c210bd3b5ae542cdb571c453087db7178f
Abhishek Pandey <ashp.pandey916@gmail.com>
2016-04-04 01:01:01 +0530

    Extended to all time steps

--- tdse.f90
+++ tdse.f90
@@ -2,10 +2,10 @@ program main
 
 use constants
 implicit none
-integer::i,j,m
-real::km,fourier
+integer::i,j,m,k
+real::km,fourier,t
 real,dimension(256)::x,v
-complex,dimension(256)::psi0,psi1,phi,diff
+complex,dimension(256)::psi0,psi1,phi,diff,psit,psit1,psit2
 complex::sumwave
 
 !calculating the values of x and V grid
@@ -56,7 +56,44 @@ psi1(i) = psi0(i) + iota*dt*(diff(i)/(2*mass)-v(i)*psi0(i))
 write(12,*)x(i),(real(psi1(i))**2 - aimag(psi1(i))**2) !This is for plotting psi square vs x grid
 enddo
 
+! Extending psi at time t=0 to all times
 
+psit = psi0
+psit1 = psi1
+t = 0.2
+do k = 1,steps
+
+! Start of fourier transform
+do m = 1,256
+sumwave = 0.0
+do j = 1,256
+sumwave = sumwave + psit(j)*exp((-1)*iota*km(m)*x(j))
+enddo
+sumwave = sumwave/(sqrt(256.0))
+phi(m) = sumwave * (km(m)**2) * (-1)
+enddo
+
+do j = 1,256
+sumwave = 0.0
+do m = 1,256
+sumwave = sumwave + phi(m)*exp(iota*km(m)*x(j))
+enddo
+sumwave = sumwave/(sqrt(256.0))
+diff(j) = sumwave
+enddo
+
+! Finding next wave packet
+do i=1,256
+psit2(i) = psit(i) - (2.0*iota*dt)*((-1)*diff(i)/(2*mass) + v(i)*psit1(i))
+if(mod(k,100).eq.0)then
+print*,'Value of t:',t,'Wavefunction:',psit2(i)
+endif
+enddo
+
+t = t+dt
+psit = psit1
+psit1 = psit2
+enddo
 
 end program main
 
