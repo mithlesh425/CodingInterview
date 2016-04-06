455baa34519d1238336967a4d8b864b3e20c3a6f
Abhishek Pandey <ashp.pandey916@gmail.com>
2016-04-06 09:09:04 +0530

    Inserted real numbers in def of iota

--- constants.f90
+++ constants.f90
@@ -2,11 +2,11 @@ module constants
 real,parameter::pi = 2.0*asin(1.0)
 real,parameter::alpha = 20.0
 real,parameter::x0 = -0.5
-real,parameter::p0 = 20.0
+real,parameter::p0 = 50.0
 real,parameter::mass = 14500.0
 real,parameter::xmin = -2.0
 real,parameter::dx = 0.02
 real,parameter::dt = 0.1
 integer,parameter::steps = 5000
-complex,parameter::iota = (0,1)
+complex,parameter::iota = (0.0,1.0)
 end module constants
